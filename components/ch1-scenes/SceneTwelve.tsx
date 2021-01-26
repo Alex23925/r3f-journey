db3ead17a8899057f472ba3c8b24384fcdce35e2
Alex23925 <alexhernandez102598@gmail.com>
2021-01-25 22:34:09 -0800

    add normal mesh

--- components/ch1-scenes/SceneTwelve.tsx
+++ components/ch1-scenes/SceneTwelve.tsx
@@ -1,12 +1,24 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect, useMemo } from 'react'
 import { MeshProps, useFrame } from 'react-three-fiber'
 import type { Mesh } from 'three'
+import * as THREE from 'three'
 import CameraControls from '../CameraControls'
+
+import alphaImg from '../../static/textures/door/alpha.jpg'
+import ambientOccImg from '../../static/textures/door/ambientOcclusion.jpg'
+import colorImg from '../../static/textures/door/color.jpg'
+import heightImg from '../../static/textures/door/height.jpg'
+import metalnessImg from '../../static/textures/door/metalness.jpg'
+import normalImg from '../../static/textures/door/normal.jpg'
+import roughnessImg from '../../static/textures/door/roughness.jpg'
+
+import gradient from '../../static/textures/gradients/3.jpg'
+import matcap from '../../static/textures/matcaps/8.png'
 
 interface SceneProps {
     elevation: number,
     color: string,
-    hoverColor:string,
+    hoverColor: string,
     wireframe: boolean
 }
 
@@ -17,59 +29,83 @@ interface BoxProps {
     wireframe: boolean,
 }
 
-const Box = (props: BoxProps) => {
-    let elevation = props.elevation
-    let color = props.color
-    let hoverColor = props.hoverColor
-    // This reference will give us direct access to the mesh
-    const mesh = useRef<Mesh>()
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => {
-        if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.001
-    })
-
-    return (
-        <mesh
-            ref={mesh}
-            position={[0, elevation ,0]}
-            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
-            <boxBufferGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial wireframe={props.wireframe} color={hovered ? hoverColor : color} />
-        </mesh>
-    )
-}
-
 
 export default function SceneTwelve(props: SceneProps) {
-    
+
     // Refs
     const sphereRef = useRef<Mesh>()
     const planeRef = useRef<Mesh>()
     const torusRef = useRef<Mesh>()
-    // Material
-    const material = <meshBasicMaterial color={props.color} />
+
+    // Textures
+    const loadingManager = useMemo(() => new THREE.LoadingManager(), [])
+
+    loadingManager.onStart = () => {
+        console.log('onStart')
+    }
+
+    loadingManager.onLoad = () => {
+        console.log('onLoaded')
+    }
+
+    loadingManager.onProgress = () => {
+        console.log('onProgress')
+    }
+
+    loadingManager.onError = () => {
+        console.log('onError')
+    }
+
+    const textureLoader = useMemo(() => new THREE.TextureLoader(loadingManager),
+        [
+            colorImg,
+            alphaImg,
+            ambientOccImg,
+            heightImg,
+            metalnessImg,
+            roughnessImg,
+            normalImg
+        ])
+
+    const doorColorTexture = textureLoader.load(colorImg)
+    const doorAlphaTexture = textureLoader.load(alphaImg)
+    const doorAmbientOcclusionTexture = textureLoader.load(ambientOccImg)
+    const doorHeightTexture = textureLoader.load(heightImg)
+    const doorMetalnessTexture = textureLoader.load(metalnessImg)
+    const doorRoughnessTexture = textureLoader.load(roughnessImg)
+    const doorNormalTexture = textureLoader.load(normalImg)
+    const matcapTexture = textureLoader.load(matcap)
+    const gradientTexture = textureLoader.load(gradient)
+
     
+
+
+    // Material
+    // const material = <meshBasicMaterial 
+    //                     map={doorColorTexture} 
+    //                     alphaMap={doorAlphaTexture}
+    //                     side={THREE.DoubleSide}
+    //                     transparent={true}
+    //                     wireframe={props.wireframe}
+    //                     color={props.color}
+    //                  />
+
+    const material = <meshNormalMaterial />
+
     // Meshes
-    const sphere = 
-        <mesh ref={sphereRef} position={[-1.5,0,0]} >
+    const sphere =
+        <mesh ref={sphereRef} position={[-1.5, 0, 0]} >
             <sphereBufferGeometry args={[.5, 16, 16]} />
             {material}
         </mesh>
 
-    const plane = 
+    const plane =
         <mesh ref={planeRef} >
-            <planeBufferGeometry args={[1,1]} />
+            <planeBufferGeometry args={[1, 1]} />
             {material}
         </mesh>
 
-    const torus = 
+    const torus =
         <mesh ref={torusRef} position={[1.5, 0, 0]} >
             <torusBufferGeometry args={[.3, .2, 16, 32]} />
             {material}
@@ -79,38 +115,25 @@ export default function SceneTwelve(props: SceneProps) {
     useFrame((state) => {
         let time = state.clock.getElapsedTime()
 
-        if(sphereRef?.current) {
+        if (sphereRef?.current && planeRef?.current && torusRef?.current) {
             sphereRef.current.rotation.y = .15 * time
-        }
-        if(planeRef?.current) {
             planeRef.current.rotation.y = .15 * time
-        }
-        if(torusRef?.current) {
             torusRef.current.rotation.y = .15 * time
         }
 
-        if (sphereRef?.current) {
+
+        if (sphereRef?.current && planeRef?.current && torusRef?.current) {
             sphereRef.current.rotation.x = .15 * time
-        }
-        if (planeRef?.current) {
             planeRef.current.rotation.x = .15 * time
-        }
-        if (torusRef?.current) {
             torusRef.current.rotation.x = .15 * time
         }
 
     })
-    
+
     return (
         <>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            {/* <Box 
-                elevation={props.elevation} 
-                color={props.color} 
-                hoverColor={props.hoverColor} 
-                wireframe={props.wireframe}  
-            /> */}
             {sphere}
             {plane}
             {torus}
